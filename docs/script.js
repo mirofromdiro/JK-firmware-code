ffa2e722784c9c77b4ced7f1aa2c585447bf6980
mirofromdiro <161164085+mirofromdiro@users.noreply.github.com>
2024-02-26 09:17:31 +0100

    Update script.js
    added countdown function

--- docs/script.js
+++ docs/script.js
@@ -27,3 +27,29 @@ function getCode()
 
 const div = document.querySelector("div");
 div.innerHTML = "<mark>"+getCode()+"</mark>";
+
+var d = new Date();
+d.setMinutes (d.getMinutes() + 30);
+d.setMinutes (0);
+
+var countDownDate =new Date();
+countDownDate.setMinutes (countDownDate.getMinutes() + 30);
+countDownDate.setMinutes (0);
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("countdown").innerHTML =  minutes + "m " + seconds + "s ";
+  
+}, 1000);
